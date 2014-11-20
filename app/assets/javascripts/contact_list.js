bb6e090179f802cdf2359ade74b40cbd17042fcd
sikanrong <sikanrong@gmail.com>
2014-11-20 04:02:12 +0100

    Whereas in my last commit the only CRUD letter covered was 'R', I now have all of "RUD", lacking only Creation functionality.
    Just adds a couple new methods and events to my marionette ItemView class to toggle "edit mode". Adds the proper server-side code to save these updates. Ensures re-rendering after client-server sync. Uses CSS class manipulation to hide and show elements when necessary.
    
    Also adds the Delete functionality, on both the client and server.
    
    Adds the proper "empty" view for display when there are no items in the list.
    
    Adds the form for item-creation which cleverly reuses the regular contact_template, passing in blank values for all expected variables. The "empty view" just renders this very item-creation form, but with an extra message above it informing the user that "there are currently no objects to display".
    
    Carefully uses underscore.js templating techniques to keep things very DRY; no template code is needlessly written twice.

--- app/assets/javascripts/contact_list.js
+++ app/assets/javascripts/contact_list.js
@@ -17,12 +17,55 @@ var ContactCollection = Backbone.Collection.extend({
 
 var SingleContactView = Backbone.Marionette.ItemView.extend({
   tagName: "li",
-  template: "#contact_template"
+  className: "display_mode",
+  
+  template: "#contact_template",
+  
+  events: {
+    "click a.destroy" : "clear",
+    "click a.toggle_edit" : "toggleEditMode",
+    "click input.save" : "toggleEditMode"
+  },
+  
+  toggleEditMode: function(){
+    if(this.$el.hasClass("display_mode")){
+      this.$el.removeClass("display_mode");
+      this.$el.addClass("edit_mode");
+    }else{
+      this.$el.addClass("display_mode");
+      this.$el.removeClass("edit_mode");
+      this.saveContact();
+    }
+  },
+  
+  saveContact: function(){
+      this.model.save({
+          full_name: this.$("#full_name").val(),
+          address_line_1: this.$("#address_line_1").val(),
+          address_line_2: this.$("#address_line_2").val(),
+          city: this.$("#city").val(),
+          state: this.$("#state").val(),
+          zip: this.$("#zip").val(),
+          extra_notes: this.$("#extra_notes").val()
+      });
+      
+      this.render();
+  },
+  
+  clear: function(){
+    this.model.destroy();
+  }
+  
+});
+
+var EmptyView = Backbone.Marionette.ItemView.extend({
+  template: "#empty_template"
 });
 
 var ContactListView = Backbone.Marionette.CollectionView.extend({
   tagName: 'ul',
-  childView: SingleContactView
+  childView: SingleContactView,
+  emptyView: EmptyView
 });
 
 //Define the main Marionette application class.
@@ -66,7 +109,7 @@ var ContactsApp = Marionette.Application.extend({
 });
 
 //initialize the global marionette application object
-var $app = new ContactsApp({container: '#main_container'});
+var $app = new ContactsApp({container: '#contacts_list'});
 
 //Start the Marionette app...
 $app.start();
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,4 +8,22 @@ class ContactsController < ApplicationController
     contact = Contact.find(:first, :conditions=>["id=?", params["id"]])
     render :text => contact.to_json, :layout=>false
   end
+  
+  def destroy
+    contact = Contact.find(:first, :conditions=>["id=?", params["id"]])
+    contact.destroy
+    render :text => contact.to_json, :layout=>false
+  end
+  
+  def update
+    contact = Contact.find(:first, :conditions=>["id=?", params["id"]])
+    contact.update(contact_params)
+    render :text => contact.to_json, :layout=>false
+  end
+  
+  private
+    
+    def contact_params
+      params.permit(:full_name, :address_line_1, :address_line_2, :city, :state, :zip, :extra_notes)
+    end
 end
